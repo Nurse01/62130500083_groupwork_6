4309bdfb7e70f37b22a34922dfcb88f6cc5555bc
Warathaya Rattachai <warathaya.nursewarathaya@mail.kmutt.ac.th>
2021-03-07 18:16:40 +0700

    [FINISH]

--- 62130500083_groupwork_6/profile.js
+++ 62130500083_groupwork_6/profile.js
@@ -1,17 +1,49 @@
+const constraints = {
+    firstname: {
+        presence: true
+    },
+    lastname: {
+        presence: true
+    },
+    email: {
+        presence: true,
+        email: true
+    },
+    gender: {
+        presence: true
+
+    },
+    phone: {
+        presence: true,
+        length: {
+            minimum: 9,
+            maximum: 10,
+            message: 'Number must between 9-10 digits'
+        },
+
+
+    },
+    chosengroup: {
+        presence: true
+    }
+}
+
+
+
 const app = {
     data() {
         return {
-            firstname: 'Warathaya',
-            lastname: 'Rattachai',
+            firstname: null,
+            lastname: null,
             location: 'Ladprao',
             imageUpper: 'images/upper.jpg',
             imageProfile: 'images/profile.jpg',
             followers: '500',
             projects: '20',
             ranks: '5',
-            fname: null,
-            lname: null,
+            email: null,
             gender: null,
+            phone: null,
             chosengroup: null,
             group_lists: [
                 { gruop_num: 1, instructor: 'Aj.Umaporn' },
@@ -19,10 +51,34 @@ const app = {
                 { gruop_num: 3, instructor: 'Aj.Sanit' },
                 { gruop_num: 4, instructor: 'Aj.Pichet' },
                 { gruop_num: 5, instructor: 'P.Toei' },
-            ]
+            ],
+            errors: null,
 
         }
-    }
+    },
+    methods: {
+        checkForm() {
+            this.errors = validate({
+                firstname: this.firstname,
+                lastname: this.lastname,
+                email: this.email,
+                gender: this.gender,
+                phone: this.phone,
+                chosengroup: this.chosengroup
+            }, constraints)
+            if (this.errors) {
+                console.log(this.errors)
+            }
+            if(!this.errors){
+                alert("Your profile is Registered successfully.")
+            }
 
+        }
+
+    },
+    updated() {
+        console.log(this.chosengroup);
+    },
 }
+
 Vue.createApp(app).mount('#app')
